33b6ddb4708dc2f4fcefed427eb71cfa646e2da4
acexcy <79228180+acexcy@users.noreply.github.com>
2026-03-16 19:42:11 +0100

    init: add base files
    Generated with 'gradle init'

---
+++ src/main/java/me/rembot/Bot.java
@@ -0,0 +1,8 @@
+package me.rembot;
+
+public class Bot {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/me/rembot/ExampleTest.java
@@ -0,0 +1,10 @@
+package me.rembot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleTest {
+    @Test void exampleMethod() {
+        assertEquals(2+2, 4);
+    }
+}
