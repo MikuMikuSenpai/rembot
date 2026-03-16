0aac676be04bf3e5b70704c53b5a623043165386
acexcy <79228180+acexcy@users.noreply.github.com>
2026-03-16 23:59:23 +0100

    refactor: make global vars public
    This is fine since they are final they wont be changed in the future

--- src/main/java/va/rembot/BotConfig.java
+++ src/main/java/va/rembot/BotConfig.java
@@ -1,6 +1,5 @@
 package va.rembot;
 
-import lombok.Getter;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,11 +8,10 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 /// The "onReady" method ensures rembot is fully loaded/started
 public class BotConfig extends ListenerAdapter {
 
-    @Getter
-    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
-    private static final String LOG_CHANNEL_ID = System.getenv("LOG_CHANNEL_ID");
-    private static final String DARWIN_CHANNEL_ID = System.getenv("DARWIN_CHANNEL_ID");
-    private static final String BANNED_WORDS = System.getenv("BANNED_WORDS");
+    public static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+    public static final String LOG_CHANNEL_ID = System.getenv("LOG_CHANNEL_ID");
+    public static final String DARWIN_CHANNEL_ID = System.getenv("DARWIN_CHANNEL_ID");
+    public static final String BANNED_WORDS = System.getenv("BANNED_WORDS");
 
     @Override
     public void onReady(ReadyEvent event) {
