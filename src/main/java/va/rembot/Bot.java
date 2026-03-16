ea2050f7ab2f72ea879f089a3dfae517330d68cb
acexcy <79228180+acexcy@users.noreply.github.com>
2026-03-16 20:20:32 +0100

    Revert "init: added basic files for starting up the bot using Docker"
    This reverts commit 5721770749017559fce782578a4d73e6459fb459.

--- src/main/java/va/rembot/Bot.java
+++ src/main/java/va/rembot/Bot.java
@@ -1,14 +1,8 @@
 package va.rembot;
 
-import net.dv8tion.jda.api.JDABuilder;
-
 public class Bot {
 
-    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
-
     public static void main(String[] args) {
-        JDABuilder
-                .createDefault(BOT_TOKEN)
-                .build();
+
     }
 }
