e8e8c85a902a0ff653c6cac89b5cdd30c7d50ed7
acexcy <79228180+acexcy@users.noreply.github.com>
2026-03-17 19:28:26 +0100

    docs: print rembot logo along with version on bot startup

--- src/main/java/va/rembot/Bot.java
+++ src/main/java/va/rembot/Bot.java
@@ -21,6 +21,7 @@ public class Bot {
                     GatewayIntent.GUILD_MESSAGES,
                     GatewayIntent.MESSAGE_CONTENT)
                     .build();
+            printLogo();
         } catch (InvalidTokenException e) {
             log.error("Bot token is invalid, check your .env file.");
         } catch (Exception e){
@@ -30,5 +31,25 @@ public class Bot {
 
         bot.addEventListener(new BotConfig());
 
+    }
+
+    private static void printLogo(){
+        // credits: https://www.asciiart.eu/text-to-ascii-art
+        // Update below configuration so that the logo can be updated correctly on new version releases
+        // Current input text "REMBOT v0.0.0 PRERELEASE"
+        // Font: standard Normal Normal 80
+        // None
+        // None
+        // Whitespace Break | Trim Whitespace
+        log.info(" ____  _____ __  __ ____   ___ _____          ___   ___   ___  ");
+        log.info("|  _ \\| ____|  \\/  | __ ) / _ \\_   _| __   __/ _ \\ / _ \\ / _ \\ ");
+        log.info("| |_) |  _| | |\\/| |  _ \\| | | || |   \\ \\ / / | | | | | | | | |");
+        log.info("|  _ <| |___| |  | | |_) | |_| || |    \\ V /| |_| | |_| | |_| |");
+        log.info("|_|_\\_\\_____|_|__|_|____/_\\___/_|_|  ___\\_/  \\___(_)___(_)___/ ");
+
+        log.info("|  _ \\|  _ \\| ____|  _ \\| ____| |   | ____|  / \\  / ___|| ____|");
+        log.info("| |_) | |_) |  _| | |_) |  _| | |   |  _|   / _ \\ \\___ \\|  _|  ");
+        log.info("|  __/|  _ <| |___|  _ <| |___| |___| |___ / ___ \\ ___) | |___ ");
+        log.info("|_|   |_| \\_\\_____|_| \\_\\_____|_____|_____/_/   \\_\\____/|_____|  ");
     }
 }
