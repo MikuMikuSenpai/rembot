7bb8e66cf4f0a81d6307cb83dabd65f7c5d6b27f
acexcy <79228180+acexcy@users.noreply.github.com>
2026-03-18 00:38:30 +0100

    feat: added proper error handling to ban
    If something goes wrong the user will be pinged and the message will be edited with the reason. I will do it like this for the remaining admin commands.

--- src/main/java/va/rembot/commands/slash/admin/Ban.java
+++ src/main/java/va/rembot/commands/slash/admin/Ban.java
@@ -1,6 +1,7 @@
 package va.rembot.commands.slash.admin;
 
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.UserSnowflake;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -17,22 +18,23 @@ public class Ban extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if (event.getName().equals("ban")){
-            event.deferReply().queue();
+            event.deferReply(true).queue();
 
             var target = event.getOption("username").getAsUser();
             var usrSnowflake = UserSnowflake.fromId(target.getId());
+            var slashCommandUser = event.getInteraction().getUser();
 
             // "reason" is an optional input, could be null so handle it:
             try {
                 var reason = event.getOption("reason").getAsString();
-                ban(event, usrSnowflake, reason);
+                ban(event, usrSnowflake, reason, slashCommandUser, target);
             } catch (NullPointerException e) {
-                ban(event, usrSnowflake, "No reason provided.");
+                ban(event, usrSnowflake, "No reason provided.", slashCommandUser, target);
             }
         }
     }
 
-    private void ban(SlashCommandInteractionEvent event, UserSnowflake usrSnowflake, String reason){
+    private void ban(SlashCommandInteractionEvent event, UserSnowflake usrSnowflake, String reason, User slashCommandUser, User targetUser){
         event.getGuild()
                 .ban(usrSnowflake, 0, TimeUnit.MINUTES)
                 .reason(reason)
@@ -43,16 +45,21 @@ public class Ban extends ListenerAdapter {
                             .and(event.getHook().deleteOriginal())
                             .queue();
                 }, new ErrorHandler()
-                        //TODO speak w miku on how we should let the user know in discord itself (currently only logs error to terminal)
                         .handle(ErrorResponse.MISSING_PERMISSIONS,
                                 e -> {
-                                    log.error("Bot doesn't have enough permissions to ban the target user. (Bot probably has a lower or same discord role hierarchy than target).");
-                                    event.getHook().deleteOriginal().queue();
+                                    log.error("Bot doesn't have enough permissions to ban the target user. (Bot probably has a lower or same discord role hierarchy as the target).");
+                                    log.error("{} tried to ban {}", slashCommandUser, targetUser);
+                                    event.getHook()
+                                            .editOriginal("Failed to ban that user because I don't have sufficient perms (most likely need a role with higher permissions than the target)." + slashCommandUser.getAsMention())
+                                            .queue();
                                 })
                         .handle(ErrorResponse.UNKNOWN_USER,
                                 e -> {
-                                    log.error("Dont know who the target user is. (Unknown user)");
-                                    event.getHook().deleteOriginal().queue();
+                                    log.error("Dont know who the target user is (Unknown user).");
+                                    log.error("{} tried to ban {}", slashCommandUser, targetUser);
+                                    event.getHook()
+                                            .editOriginal("I can't find the person you are trying to ban (unknown user)." + slashCommandUser.getAsMention())
+                                            .queue();
                                 }));
     }
 }
