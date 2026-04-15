850173d77ef8c1fee61e3fe938147970a788abc7
acexcy <79228180+acexcy@users.noreply.github.com>
2026-04-15 06:01:20 +0200

    refactor: rewrote the whole thing its better now

--- src/main/java/va/rembot/other/highlight/HighlightedMessage.java
+++ src/main/java/va/rembot/other/highlight/HighlightedMessage.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.GenericMessageReactionEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -29,7 +30,6 @@ public class HighlightedMessage extends ListenerAdapter {
     private static String starEmojiUnicode = "U+2B50";
 
     //TODO make exceptions and add them at the orelsethrow, cba atm
-    //TODO there is some repeated code extract those to a method (e.g. embedbuilder)
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
 
@@ -40,40 +40,33 @@ public class HighlightedMessage extends ListenerAdapter {
 
         if (emojiAsUnicode.equalsIgnoreCase(starEmojiUnicode)) {
 
-            var msgId = event.getMessageIdLong();
+            long msgId = event.getMessageIdLong();
             var starMsgDao = new StarMessageDao();
             var msgDao = new MessageDao();
             starMsgDao.create(new StarMessage(msgId, 0, false, 0));//init the bs thing
 
-            var starAmount = 0;
-            starAmount = starMsgDao.get(msgId)
+            int starAmount = starMsgDao.get(msgId)
                     .orElseThrow()
                     .starAmount();
 
-            var embedMsgId = starMsgDao.get(msgId)
+            long embedMsgId = starMsgDao.get(msgId)
                     .orElseThrow()
                     .embedMessageId();
 
-            var isSent = starMsgDao.get(msgId).orElseThrow().isSent();
+            boolean isSent = starMsgDao.get(msgId)
+                    .orElseThrow()
+                    .isSent();
 
-            var newStarAmount = starAmount + 1;
+            int newStarAmount = starAmount + 1;
 
-            // update w new star amoutn
+            // update w new star amount
             starMsgDao.update(new StarMessage(msgId, newStarAmount, isSent, embedMsgId));
 
-            var msgContent = msgDao.get(msgId)
+            String msgContent = msgDao.get(msgId)
                    .orElseThrow()
                    .messageContent();
 
-            //https://stackoverflow.com/a/237068
-            //i made the regex, if anyone knows how to optimize, feel free but i think this is p good,
-            //we grab any URL that ends w specific file extension like .gif,... we include optional
-            // query bs and fragment from the url since discord's cdn likes too spam those
-            //another note i feel like case_insensitive is bandage fix but we should be fine
-            // (we need this in case a file format would be written in uppercase)
-            // we accept:
-            //   video: MP4, MOV, MKV, AVI, WEBM
-            //   picture: JPEG, PNG, GIF, HEIC, WEBP
+            //extract media URL from message
             Pattern pattern = Pattern.compile("https?\\S+" +
                     "(\\.avi|" +
                     "\\.gif|" +
@@ -97,11 +90,11 @@ public class HighlightedMessage extends ListenerAdapter {
                 hasMediaLink = false;
             }
 
-            var msgAuthorId = msgDao.get(msgId)
+            long msgAuthorId = msgDao.get(msgId)
                     .orElseThrow()
                     .discordId();
 
-            var attachmentLinks = msgDao.get(msgId)
+            String attachmentLinks = msgDao.get(msgId)
                     .orElseThrow()
                     .attachmentsLinks();
 
@@ -114,39 +107,25 @@ public class HighlightedMessage extends ListenerAdapter {
 
             var user = event.getJDA().getUserById(msgAuthorId);
 
-            //notttgointolie no fucking idea why we have to check the msgcontent
-            // for it to not be empty forgot how i stumbled onto this ig keep it since it doesnt hurt,
-            // issent is to prevent dupes/duplicate msg highlights in darwin
             if (newStarAmount >= BotConfig.getHighlightStarThresholdInt()){
 
+                String stars = Integer.valueOf(newStarAmount).toString();
+                EmbedBuilder embedHighlight =  getBaseEmbedMessage(user, stars, msgContent, event);
+                TextChannel darwinChannel = event.getGuild()
+                        .getChannelById(TextChannel.class, BotConfig.DARWIN_CHANNEL_ID);
+
+                //prevent sending duplicates
                 if (!isSent) {
 
                     isSent = true;
 
-                    String stars = Integer.valueOf(newStarAmount).toString();
-
-                    EmbedBuilder embedHighlight = new EmbedBuilder();
-                    embedHighlight.setTitle("⭐ message highlight ⭐");
-                    embedHighlight.addField("user", user.getAsMention(), true);
-                    embedHighlight.addField("stars", stars, true);
-                    embedHighlight.setColor(0xffcd3c);
-
-                    var darwinChannel = event.getGuild()
-                            .getChannelById(TextChannel.class, BotConfig.DARWIN_CHANNEL_ID);
-
                     if (!(msgContent.length() > 1024)) {
-
-                        if (!msgContent.isEmpty()) {
-                            embedHighlight.addField("msg", msgContent, false);
-                        } else {
-                            embedHighlight.addField("msg", "Original msg was empty. (Check attachments below)", false);
-                        }
 
                         boolean finalIsSent = isSent;
                         darwinChannel
                                 .sendMessageEmbeds(embedHighlight.build())
                                 .queue(message -> {
-                                    var newEmbedMsgId = message.getIdLong();
+                                    long newEmbedMsgId = message.getIdLong();
                                     starMsgDao.update(new StarMessage(msgId, newStarAmount, finalIsSent, newEmbedMsgId));
 
                                     if (hasAttachments || hasMediaLink)
@@ -154,8 +133,6 @@ public class HighlightedMessage extends ListenerAdapter {
 
                                 });
                     } else {
-
-                        embedHighlight.addField("msg", "Message was too long check attached file for content", false);
 
                         try {
                             var myFile = new File("long_message.txt");
@@ -194,36 +171,9 @@ public class HighlightedMessage extends ListenerAdapter {
                     //if message was already sent (we edit to update stars amount instead of sending a new embed):
                 } else {
 
-                    String stars = Integer.valueOf(newStarAmount).toString();
-
-                    EmbedBuilder embedHighlight = new EmbedBuilder();
-                    embedHighlight.setTitle("⭐ message highlight ⭐");
-                    embedHighlight.addField("user", user.getAsMention(), true);
-                    embedHighlight.addField("stars", stars, true);
-                    embedHighlight.setColor(0xffcd3c);
-
-                    var darwinChannel = event
-                            .getGuild()
-                            .getChannelById(TextChannel.class, BotConfig.DARWIN_CHANNEL_ID);
-
-                    if (!(msgContent.length() > 1024)) {
-
-                        if (!msgContent.isEmpty()) {
-                            embedHighlight.addField("msg", msgContent, false);
-                        } else {
-                            embedHighlight.addField("msg", "Original msg was empty. (Check attachments below)", false);
-                        }
-
-                        darwinChannel
-                                .editMessageEmbedsById(embedMsgId, embedHighlight.build())
-                                .queue();
-                    } else {
-
-                        embedHighlight.addField("msg", "Message was too long check attached file for content", false);
-                        darwinChannel
-                                .editMessageEmbedsById(embedMsgId, embedHighlight.build())
-                                .queue();
-                    }
+                    darwinChannel
+                            .editMessageEmbedsById(embedMsgId, embedHighlight.build())
+                            .queue();
                 }
            }
         }
@@ -239,50 +189,39 @@ public class HighlightedMessage extends ListenerAdapter {
 
         if (emojiAsUnicode.equalsIgnoreCase(starEmojiUnicode)) {
 
-            var msgId = event.getMessageIdLong();
+            long msgId = event.getMessageIdLong();
             var starMsgDao = new StarMessageDao();
             var msgDao = new MessageDao();
-            var starAmount = starMsgDao.get(msgId).orElseThrow().starAmount();
-            var isSent = starMsgDao.get(msgId).orElseThrow().isSent();
-            var newStarAmount = starAmount - 1;
-            var msgContent = msgDao.get(msgId).orElseThrow().messageContent();
+            int starAmount = starMsgDao.get(msgId)
+                    .orElseThrow()
+                    .starAmount();
+            boolean isSent = starMsgDao.get(msgId)
+                    .orElseThrow()
+                    .isSent();
+            int newStarAmount = starAmount - 1;
+            String msgContent = msgDao.get(msgId)
+                    .orElseThrow()
+                    .messageContent();
 
             String stars = Integer.valueOf(newStarAmount).toString();
 
-            var msgAuthorId = msgDao.get(msgId)
+            long msgAuthorId = msgDao.get(msgId)
                     .orElseThrow()
                     .discordId();
 
             var user = event.getJDA().getUserById(msgAuthorId);
 
-            EmbedBuilder embedHighlight = new EmbedBuilder();
-            embedHighlight.setTitle("⭐ message highlight ⭐");
-            embedHighlight.addField("user", user.getAsMention(), true);
-            embedHighlight.addField("stars", stars, true);
-            embedHighlight.setColor(0xffcd3c);
+            EmbedBuilder embedHighlight = getBaseEmbedMessage(user, stars, msgContent, event);
 
-            var embedMsgId = starMsgDao.get(msgId).orElseThrow().embedMessageId();
+            long embedMsgId = starMsgDao
+                    .get(msgId)
+                    .orElseThrow()
+                    .embedMessageId();
+            TextChannel darwinChannel = event.getGuild().getChannelById(TextChannel.class, BotConfig.DARWIN_CHANNEL_ID);
 
-            var darwinChannel = event.getGuild().getChannelById(TextChannel.class, BotConfig.DARWIN_CHANNEL_ID);
-
-            if (!(msgContent.length() > 1024)) {
-
-                if (!msgContent.isEmpty()) {
-                    embedHighlight.addField("msg", msgContent, false);
-                } else {
-                    embedHighlight.addField("msg", "Original msg was empty. (Check attachments below)", false);
-                }
-
-                darwinChannel
-                        .editMessageEmbedsById(embedMsgId, embedHighlight.build())
-                        .queue();
-            } else {
-
-                embedHighlight.addField("msg", "Message was too long check attached file for content", false);
-                darwinChannel
-                        .editMessageEmbedsById(embedMsgId, embedHighlight.build())
-                        .queue();
-            }
+            darwinChannel
+                    .editMessageEmbedsById(embedMsgId, embedHighlight.build())
+                    .queue();
 
             starMsgDao.update(new StarMessage(msgId, newStarAmount, isSent, embedMsgId));
         }
@@ -312,5 +251,33 @@ public class HighlightedMessage extends ListenerAdapter {
 
         userDao.create(new User(discordId));
         msgDao.create(new Message(discordMsgId, discordId, new Timestamp(msgTimeCreated), msgContent, messageWithLinks));
+    }
+
+    //TODO fix name collision change our model's name User to something else;
+    // clashes with jda's User class
+    private static EmbedBuilder getBaseEmbedMessage(net.dv8tion.jda.api.entities.User user, String stars, String messageContent, GenericMessageReactionEvent event) {
+
+        //TODO check that user isnt NULL, e.g. since we dont store bots atm at least their ID isnt in our DB,
+        // when we call the bulshit below itll throw nullpointer exception,
+        // rembot doesnt crash but the error is not being handled, host could get confused if they spot the error in terminal
+        EmbedBuilder embedHighlight = new EmbedBuilder();
+        embedHighlight.setTitle("⭐ message highlight ⭐");
+        embedHighlight.addField("user", user.getAsMention(), true);
+        embedHighlight.addField("stars", stars, true);
+        embedHighlight.setColor(0xffcd3c);
+
+        if (!(messageContent.length() > 1024)) {
+
+            if (!messageContent.isEmpty()) {
+                embedHighlight.addField("msg", messageContent, false);
+            } else {
+                embedHighlight.addField("msg", "Original msg was empty. (Check attachments below)", false);
+            }
+
+        } else {
+            embedHighlight.addField("msg", "Message was too long check attached file for content", false);
+        }
+
+        return embedHighlight;
     }
 }
